0c49519c0723713698810678e61d60e98508ee39
Kenneth Danso <kwadwobaahdanso@gmail.com>
2024-05-20 14:32:50 +0000

    chore: updates routes

--- src/router/AppRouter.tsx
+++ src/router/AppRouter.tsx
@@ -1,11 +1,13 @@
 import {Route, Routes, Navigate} from 'react-router-dom'
 import LandingPage from '../pages/LandingPage'
+import InformationPage from '../pages/InformationPage'
 
 function AppRouter() {
   return (
     <Routes>
         <Route path="/" element={<Navigate to="drip" replace/>}/>
         <Route path="drip" element={<LandingPage/>}/>
+        <Route path="/drip/information" element={<InformationPage/>}/>
     </Routes>
   )
 }
