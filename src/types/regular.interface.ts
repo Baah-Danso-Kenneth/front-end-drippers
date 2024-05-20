959b1fd6c5bd59fb374a4c507c34c161b9ac8f2c
Kenneth Danso <kwadwobaahdanso@gmail.com>
2024-05-20 14:33:22 +0000

    chores: interfaces for project included

---
+++ src/types/regular.interface.ts
@@ -0,0 +1,27 @@
+import { ReactNode } from "react";
+
+export interface LayoutProps {
+    children: ReactNode;
+}
+
+
+export interface CategoryProps {
+    id: number;
+    department: string;
+    image: string;
+}
+
+export interface QuestionProps {
+    id:number;
+    title: string;
+    description: string;
+    icons: ReactNode;
+}
+
+export interface NewsProps{
+    id: number;
+    title: string;
+    description:string;
+    image: string;
+    notification: string;
+}
