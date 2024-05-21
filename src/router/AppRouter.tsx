e917b717805788ac85ae209e1e3773fd980e74e9
Kenneth Danso <kwadwobaahdanso@gmail.com>
2024-05-21 22:32:50 +0000

    feat: Welcomepage for various users
    Logically will direct users based on their interest or gender to that particular page ie.women, men and kids

--- src/router/AppRouter.tsx
+++ src/router/AppRouter.tsx
@@ -4,6 +4,7 @@ import InformationPage from '../pages/InformationPage'
 import Login from '../pages/auth/Login'
 import Register from '../pages/auth/Register'
 import ForgotPassword from '../pages/auth/ForgotPassword'
+import WelcomePage from '../pages/WelcomePage'
 
 function AppRouter() {
   return (
@@ -16,6 +17,12 @@ function AppRouter() {
         <Route path="/drip/login" element={<Login/>}/>
         <Route path="/drip/register" element={<Register/>}/>
         <Route path="/drip/forgot-password" element={<ForgotPassword/>}/>
+
+        <Route path="/welcome/drip" element={<WelcomePage/>}>
+            <Route path="women" element={<h1>women</h1>}/>
+            <Route path="men" element={<h1>men</h1>}/>
+            <Route path="kids" element={<h1>kids</h1>}/>
+        </Route>
 
     </Routes>
   )
