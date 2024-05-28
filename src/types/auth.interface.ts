247a77d1d7bf9b2bee2027b6762c0042ff5217cb
Kenneth Danso <kwadwobaahdanso@gmail.com>
2024-05-28 15:51:51 +0000

    feat: types for authentication Props in redux

---
+++ src/types/auth.interface.ts
@@ -0,0 +1,52 @@
+import { AxiosInstance } from "axios";
+
+export interface User{
+    username: string;
+    first_name: string;
+    last_name: string;
+}
+
+export interface RegisterUserData{
+    username: string;
+    email: string;
+    password: string;
+}
+
+export interface LoginUserData{
+    email: string;
+    password: string;
+}
+
+export interface ActivateUserData{
+    uid: string;
+    token: string;
+}
+
+export interface AuthResponseData{
+    user: User;
+    access: string;
+    refresh: string;
+    [key: string]: unknown;
+}
+
+export interface AxiosError{
+    reponse? : {
+        data?: {
+            message?: string;
+        };
+    };
+    message:string;
+}
+
+
+export function isAxiosError(error: unknown): error is AxiosInstance{
+    return (error as AxiosError).message !== undefined
+}
+
+export interface AuthState{
+    user: User | null;
+    isError: boolean;
+    isLoading: boolean;
+    isSuccess: boolean;
+    message: string;
+}
